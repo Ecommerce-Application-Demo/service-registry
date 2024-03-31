09176d78826d69e440afa147f234174a708e842c
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-03-31 12:01:09 +0530

    Enable scheduling

--- src/main/java/com/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/serviceregistry/ServiceRegistryApplication.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableScheduling
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
